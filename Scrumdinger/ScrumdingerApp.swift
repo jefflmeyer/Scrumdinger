d0bf505e4fa791328a7f1b363d9cd3475eac596c
jefflmeyer <jeff.meyer234@gmail.com>
2023-11-24 17:22:39 -0600

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Jeff Meyer on 11/24/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
