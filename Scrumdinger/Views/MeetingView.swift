81d3c97d0cdefe1a4a53ba976746b4a539f24d51
jefflmeyer <jeff.meyer234@gmail.com>
2023-11-30 17:00:28 -0800

    Import project files, create overlay view in MeetingView

---
+++ Scrumdinger/Views/MeetingView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Jeff Meyer on 11/24/23.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    @Binding var scrum: DailyScrum
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 16.0)
+                .fill(scrum.theme.mainColor)
+            VStack {
+                ProgressView(value: 10, total: 30)
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("Seconds Elapsed")
+                            .font(.caption)
+                        Label("300", systemImage: "hourglass.tophalf.fill")
+                    }
+                    Spacer()
+                    VStack(alignment: .trailing) {
+                        Text("Seconds Remaining")
+                            .font(.caption)
+                        Label("600", systemImage: "hourglass.bottomhalf.fill")
+                        
+                    }
+                }
+                .accessibilityElement(children: /*@START_MENU_TOKEN@*/.ignore/*@END_MENU_TOKEN@*/)
+                .accessibilityLabel("Time Remaining")
+                .accessibilityValue("10 minutes")
+                Circle()
+                    .strokeBorder(lineWidth: 24)
+                HStack {
+                    Text("Speaker 1 of 3")
+                    Spacer()
+                    Button(action: {}) {
+                        Image(systemName: "forward.fill")
+                    }
+                    .accessibilityLabel("Next speaker")
+                }
+            }
+        }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    MeetingView(scrum: .constant(DailyScrum.sampleData[0]))
+}
